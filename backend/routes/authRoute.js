bde146d852e7dcc145df26b70d82e1ef7c69d8b1
eyobScript <eyobbmulugeta@gmail.com>
2024-12-16 19:47:49 +0300

    add login route to routes directory

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createUser } from '../controller/userCtrl.js'
+import { createUser, loginUserCtrl } from '../controller/userCtrl.js'
 const router = express.Router();
 
 router.post('/register', createUser);
+router.post('/login', loginUserCtrl);
 
 
 export default router;
