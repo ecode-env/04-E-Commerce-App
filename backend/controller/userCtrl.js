5e9a76345411c507cf5b6137a0c49f02c3fd745c
eyobScript <eyobbmulugeta@gmail.com>
2024-12-16 19:47:08 +0300

    add login ctrl

--- backend/controller/userCtrl.js
+++ backend/controller/userCtrl.js
@@ -1,5 +1,5 @@
-import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
@@ -10,23 +10,27 @@ const createUser = asyncHandler(async (req, res) => {
     res.status(201).json(newUser);
   } else {
     // If user already exists, throw an error message
-    throw new Error('User already exists');
+    throw new Error("User already exists");
   }
 });
 
-// const createUser = async (req, res) => {
-//     const email = req.body.email;
-//     const findUser = await User.findOne({ email });
-//     if (!findUser) {
-//         try {
-//             const newUser = await User.create(req.body);
-//             res.status(201).json({ message: 'User created successfully', user: newUser });
-//         } catch (error) {
-//             res.status(500).json({ error: 'An error occurred while creating the user' });
-//         }
-//     } else {
-//         res.status(400).json({ error: 'User with this email already exists' });
-//     }
-// };
 
-export { createUser };
+
+// login user controller
+const loginUserCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Find user by email
+  const findUser = await User.findOne({ email });
+
+  // Check if user exists and password matches
+  if (findUser && await findUser.isPasswordMatch(password)) {
+    // Password matches, send user data or token
+    res.json(findUser);
+  } else {
+    // Invalid credentials
+    throw new Error("Invalid Credentials");
+  }
+});
+
+export { createUser, loginUserCtrl };
