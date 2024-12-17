73cb97ddd1488a839f8475d44582527682cb20c4
eyobScript <eyobbmulugeta@gmail.com>
2024-12-17 12:00:34 +0300

    setup generate web token

--- backend/config/jwtToken.js
+++ backend/config/jwtToken.js
@@ -1,1 +1,7 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+};
+
+export default generateToken;
