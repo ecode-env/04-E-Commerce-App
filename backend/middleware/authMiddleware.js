4c2240062e67cb8a975242147051ab5c9ef5d1eb
eyobScript <eyobbmulugeta@gmail.com>
2024-12-17 18:41:50 +0300

    initialize if the token exist

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,39 @@
+import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  let jwtToken;
+  if (req?.headers?.authorization?.startsWith("Bearer ")) {
+    jwtToken = req?.headers?.authorization?.split(" ")[1];
+    try {
+      if (jwtToken) {
+        const decodedToken = jwt.verify(jwtToken, process.env.JWT_SECRET);
+        const user = await User.findById(decodedToken?.id);
+        req.user = user;
+        next();
+      }
+    } catch (error) {
+      throw new Error(
+        "Not Authorized token received, Please try logging in again"
+      );
+    }
+  } else {
+    throw new Error("There is no token attached to the header");
+  }
+});
+
+const isAdmin = asyncHandler(async (req, res, next) => {
+    // Destructure the user object added to the request
+    const { email } = req.user;
+  
+    // Find the user by email in the database
+    const isAdminUser = await User.findOne({ email });
+  
+    // Check if user exists and has an admin role
+    if (!isAdminUser || isAdminUser.role !== "admin") throw new Error("Unauthorized access, only admin users are allowed");
+    // If user is admin, proceed to the next middleware or route
+    next();
+  });
+
+export { authMiddleware, isAdmin};
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,12 +1,21 @@
-import express from 'express';
-import { createUser, loginUserCtrl, getAllUsers, getSingleUser, deleteUser, updateUser } from '../controller/userCtrl.js'
+import express from "express";
+import {
+  createUser,
+  loginUserCtrl,
+  getAllUsers,
+  getSingleUser,
+  deleteUser,
+  updateUser,
+} from "../controller/userCtrl.js";
+import { authMiddleware, isAdmin } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
-router.post('/register', createUser);
-router.post('/login', loginUserCtrl);
-router.get('/all-users',getAllUsers);
-router.get('/:id', getSingleUser)
-router.delete('/:id', deleteUser);
-router.put('/:id', updateUser)
+router.post("/register", createUser);
+router.post("/login", loginUserCtrl);
+router.get("/all-users", getAllUsers);
+router.get("/:id", authMiddleware, isAdmin, getSingleUser);
+router.delete("/:id", deleteUser);
+router.put("/:id", updateUser);
 
 export default router;
