f4d9f6e28fa3b4241120ab1988c0e25120c30a93
Eyob <eyobbmulugta@gmail.com>
2024-12-24 15:14:11 +0300

    initialize mogodb schema for product.

---
+++ backend/models/productModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    brand: {
+      type: String,
+      enum: ["Apple", "Samsung", "Lenovo"],
+    },
+    quantity: Number,
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+      required: true,
+    },
+    color: {
+      type: String,
+      enum: ["Black", "Red", "Brown"],
+    },
+    rating: [
+      {
+        star: Number,
+        postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      },
+    ],
+  },
+  { timeseries: true }
+);
+
+//Export the model
+module.exports = mongoose.model("Product", ProductSchema);
