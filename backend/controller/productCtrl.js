efed3e8115f112a3bd6c7e94cf82a5685467af92
Eyob <eyobbmulugta@gmail.com>
2024-12-24 16:50:58 +0300

    its product controller

--- backend/controller/productCtrl.js
+++ backend/controller/productCtrl.js
@@ -1,1 +1,24 @@
-import Product from '../models/productModel.js';
+import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
+
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+
+const getProducts = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const findProduct = await Product.findById(id);
+        res.status(201).json(findProduct);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+export { createProduct, getProducts };
