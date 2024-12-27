6fe213bbcb2d60d652a77e51345680461bd0f10c
Eyob <eyobbmulugta@gmail.com>
2024-12-27 19:24:12 +0300

    initialized blog model.

---
+++ backend/models/blogModel.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+const { Schema, model, Types } = mongoose;
+
+// Declare the Schema of the Mongo model
+var blogSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    numView:{
+        type:Number,
+        default: 0,
+    },
+    isLiked: {
+        type: Boolean,
+        default: false,
+    },
+    isDisLiked: {
+        type: Boolean,
+        default: false,
+    },
+    likes : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    disLikes : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    image: {
+        type: String,
+        default: 'https://thumbs.dreamstime.com/z/blogging-blog-concepts-ideas-worktable-blogging-blog-concepts-ideas-white-worktable-110423482.jpg?ct=jpeg'
+    },
+    author: {
+        type: String,
+        default: 'Admin'
+    }
+});
+
+//Export the model
+const Blog = model('Blog', blogSchema);
+export default Blog;
