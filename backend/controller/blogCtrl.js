c0e8f060c8fe7cd877b6aff2025838729db05055
eyobScript <eyobbmulugeta@gmail.com>
2024-12-29 13:43:08 +0300

    set the update blog controller

--- backend/controller/blogCtrl.js
+++ backend/controller/blogCtrl.js
@@ -4,7 +4,7 @@ import asyncHandler from "express-async-handler";
 import validateMongoDBid from '../utils/validateMongodbid.js'
 
 
-
+// Create a new blog
 const createBlog = asyncHandler(async (req, res) => {
     try {
         const newBlog = await Blog.create(req.body);
@@ -15,4 +15,17 @@ const createBlog = asyncHandler(async (req, res) => {
 });
 
 
-export default createBlog;
+// Update an existing blog.
+
+const updateBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {new: true});
+        res.json(updateBlog);
+    } catch (error) {
+        throw new Error(error);
+        
+    }
+});
+
+export { createBlog, updateBlog };
--- backend/routes/blogRouter.js
+++ backend/routes/blogRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { authMiddleware, isAdmin } from '../middleware/authMiddleware.js';
-import createBlog from '../controller/blogCtrl.js';
+import {createBlog, updateBlog} from '../controller/blogCtrl.js';
 
 router.post('/',authMiddleware ,isAdmin,createBlog);
+router.put('/:id',authMiddleware ,isAdmin,updateBlog);
 
 export default router;
