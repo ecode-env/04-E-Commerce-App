7ca805b46e9a9809be4a28a69af88649e6203513
eyobScript <eyobbmulugeta@gmail.com>
2024-12-29 15:00:11 +0300

    get one blog and increase the number of view.

--- backend/controller/blogCtrl.js
+++ backend/controller/blogCtrl.js
@@ -1,31 +1,44 @@
 import Blog from "../models/blogModel.js";
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
-import validateMongoDBid from '../utils/validateMongodbid.js'
-
+import validateMongoDBid from "../utils/validateMongodbid.js";
 
 // Create a new blog
 const createBlog = asyncHandler(async (req, res) => {
-    try {
-        const newBlog = await Blog.create(req.body);
-        res.json(newBlog);
-    } catch (error) {
-        throw new Error(error);
-    }
+  try {
+    const newBlog = await Blog.create(req.body);
+    res.json(newBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
-
 
 // Update an existing blog.
 
 const updateBlog = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    try {
-        const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {new: true});
-        res.json(updateBlog);
-    } catch (error) {
-        throw new Error(error);
-        
-    }
+  const { id } = req.params;
+  try {
+    const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updateBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
-export { createBlog, updateBlog };
+// get the blog and increase numViews by one.
+
+const getBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const getBlog = await Blog.findById(id);
+    await Blog.findByIdAndUpdate(id, { $inc: { numView: 1 } }, { new: true });
+    res.json(getBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export { createBlog, updateBlog, getBlog };
--- backend/routes/blogRouter.js
+++ backend/routes/blogRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { authMiddleware, isAdmin } from '../middleware/authMiddleware.js';
-import {createBlog, updateBlog} from '../controller/blogCtrl.js';
+import {createBlog, updateBlog, getBlog} from '../controller/blogCtrl.js';
 
 router.post('/',authMiddleware ,isAdmin,createBlog);
 router.put('/:id',authMiddleware ,isAdmin,updateBlog);
+router.get('/:id' ,getBlog);
 
 export default router;
