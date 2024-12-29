c097bd9e2ba1312fb46c8365642bea6903eb73ff
eyobScript <eyobbmulugeta@gmail.com>
2024-12-29 11:28:28 +0300

    initialized create blog ctrl

--- backend/controller/blogCtrl.js
+++ backend/controller/blogCtrl.js
@@ -2,3 +2,10 @@ import Blog from "../models/blogModel";
 import User from "../models/userModel";
 import asyncHandler from "express-async-handler";
 import validateMongoDBid from '../utils/validateMongodbid.js'
+
+
+
+const createBlog = asyncHandler(async (req, res) => {});
+
+
+export default createBlog;
