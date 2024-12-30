a10a1a59b33924271404a4a1cfba3843f9c86ccf
eyobScript <eyobbmulugeta@gmail.com>
2024-12-30 17:36:39 +0300

    updated

--- backend/routes/productCategoryRoute.js
+++ backend/routes/productCategoryRoute.js
@@ -1,16 +1,13 @@
-import express from 'express';
-import { createCategory } from '../controller/productCategoryCtrl.js';
-import {authMiddleware, isAdmin} from '../middleware/authMiddleware.js'
-
-
+import express from "express";
+import {
+  createCategory,
+  updateCategory,
+} from "../controller/productCategoryCtrl.js";
+import { authMiddleware, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-
-router.post('/',authMiddleware,isAdmin, createCategory);
-
-
-
-
+router.post("/", authMiddleware, isAdmin, createCategory);
+router.put("/:id", authMiddleware, isAdmin, updateCategory);
 
 export default router;
