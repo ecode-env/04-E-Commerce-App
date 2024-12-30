ae5383a0c3fd12051a9a38d426518f7c87cf30f2
eyobScript <eyobbmulugeta@gmail.com>
2024-12-30 17:34:33 +0300

    created category updater.

--- backend/controller/productCategoryCtrl.js
+++ backend/controller/productCategoryCtrl.js
@@ -1,24 +1,30 @@
-import asyncHandler from 'express-async-handler';
-import Category from '../models/productCategoryModel.js';
-import validateMongoDBid from '../utils/validateMongodbid.js'
-
+import asyncHandler from "express-async-handler";
+import Category from "../models/productCategoryModel.js";
+import validateMongoDBid from "../utils/validateMongodbid.js";
 
 // Create a new product category
 
 const createCategory = asyncHandler(async (req, res) => {
-    try {
-        const newCategory = await Category.create(req.body);
-        res.status(201).json(newCategory);
-    } catch (error) {
-        throw new Error(error);
-    }
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
+// Update a product category
 
+const updateCategory = asyncHandler(async (req, res) => {
+  const id = req.params;
+  try {
+    const updateCategory = await Category.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updateCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-
-
-
-
-
-export {createCategory};
+export { createCategory };
