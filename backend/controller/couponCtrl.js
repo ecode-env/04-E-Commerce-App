d881864b9f4183ef688a96829cd4dcf7956c0c35
eyobScript <eyobbmulugeta@gmail.com>
2024-12-31 17:53:38 +0300

    create create coupon function.

--- backend/controller/couponCtrl.js
+++ backend/controller/couponCtrl.js
@@ -1,3 +1,13 @@
 import Coupon from "../models/couponModel";
 import validateMongoDBid from "../utils/validateMongodbid.js";
 import asyncHandler from "express-async-handler";
+
+// Create a new coupon
+const createCoupon = asyncHandler(async (req, res) => {});
+
+
+
+
+
+
+export { createCoupon, updateCoupon, deleteCoupon, getCoupon, getAllCoupons };
