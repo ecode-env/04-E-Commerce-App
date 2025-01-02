87486f54dc2fdedad72251fae7a6179706b05db3
eyobScript <eyobbmulugeta@gmail.com>
2025-01-02 19:04:19 +0300

    update

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,16 +8,26 @@ import {
   deleteProduct,
   addToWishList,
   rating,
+  uploadImages,
 } from "../controller/productCtrl.js";
+import { productImgResize, uploadPhoto } from "../middleware/uploadImages.js";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createProduct);
+router.put(
+  "/upload/:id",
+  authMiddleware, // Authenticate user
+  isAdmin, // Check for admin privileges
+  uploadPhoto.array("images", 10), // Upload up to 10 files
+  productImgResize, // Resize and process images
+  uploadImages
+);
 router.get("/:id", getProducts);
 router.get("/", getAllProducts);
 router.post("/:id", authMiddleware, isAdmin, updateProduct);
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
-router.put('/wishlist',authMiddleware, addToWishList)
-router.put('/rating',authMiddleware, rating)
+router.put("/wishlist", authMiddleware, addToWishList);
+router.put("/rating", authMiddleware, rating);
 
 export default router;
