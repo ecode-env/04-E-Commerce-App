c2bbf4fb89002265add3778a8429770008700dd3
eyobScript <eyobbmulugeta@gmail.com>
2025-01-03 17:27:33 +0300

    add route

--- backend/routes/blogRouter.js
+++ backend/routes/blogRouter.js
@@ -1,14 +1,32 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import { authMiddleware, isAdmin } from '../middleware/authMiddleware.js';
-import {createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog, likeBlog, dislikeBlog} from '../controller/blogCtrl.js';
+import { authMiddleware, isAdmin } from "../middleware/authMiddleware.js";
+import {
+  createBlog,
+  updateBlog,
+  getBlog,
+  getAllBlogs,
+  deleteBlog,
+  likeBlog,
+  dislikeBlog,
+  uploadImages,
+} from "../controller/blogCtrl.js";
+import { blogImgResize, uploadPhoto } from "../middleware/uploadImages.js";
 
-router.post('/',authMiddleware ,isAdmin,createBlog);
-router.put('/:id',authMiddleware ,isAdmin,updateBlog);
-router.get('/:id' ,getBlog);
-router.get('/' ,getAllBlogs);
-router.delete('/:id',authMiddleware ,isAdmin ,deleteBlog);
-router.patch('/likes',authMiddleware ,isAdmin ,likeBlog);
-router.patch('/dislikes',authMiddleware ,isAdmin ,dislikeBlog);
+router.post("/", authMiddleware, isAdmin, createBlog);
+router.put(
+  "/upload/:id",
+  authMiddleware, // Authenticate user
+  isAdmin, // Check for admin privileges
+  uploadPhoto.array("images", 2), // Upload up to 10 files
+  blogImgResize, // Resize and process images
+  uploadImages
+);
+router.put("/:id", authMiddleware, isAdmin, updateBlog);
+router.get("/:id", getBlog);
+router.get("/", getAllBlogs);
+router.delete("/:id", authMiddleware, isAdmin, deleteBlog);
+router.patch("/likes", authMiddleware, isAdmin, likeBlog);
+router.patch("/dislikes", authMiddleware, isAdmin, dislikeBlog);
 
 export default router;
