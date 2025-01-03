2d6c4d9d9622d5702c7a9898c37809c9b610f454
eyobScript <eyobbmulugeta@gmail.com>
2025-01-03 17:15:36 +0300

    images

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -36,10 +36,7 @@ var blogSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }],
-    image: {
-        type: String,
-        default: 'https://thumbs.dreamstime.com/z/blogging-blog-concepts-ideas-worktable-blogging-blog-concepts-ideas-white-worktable-110423482.jpg?ct=jpeg'
-    },
+    images: [],
     author: {
         type: String,
         default: 'Admin'
