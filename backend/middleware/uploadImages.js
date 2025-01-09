6c15787199ec51a030f52e3ca2bb6177144f157a
eyobScript <eyobbmulugeta@gmail.com>
2025-01-09 12:03:10 +0300

    set our productImgResize function fro resize the image.

--- backend/middleware/uploadImages.js
+++ backend/middleware/uploadImages.js
@@ -8,18 +8,15 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const multerStorage = multer.diskStorage({
   destination: function (req, res, cb) {
-    cb(null, path.join(__dirname, '../public/images'))
+    cb(null, path.join(__dirname, "../public/images"));
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + '.jpeg');
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ".jpeg");
   },
 });
-
-
 
 // Filter files by MIME type
 const multerFilter = (req, file, cb) => {
@@ -39,8 +36,17 @@ const uploadPhoto = multer({
 
 // Product Image Resize Middleware
 const productImgResize = async (req, res, next) => {
+  if (!req.files) return next();
+  await Promise.all(
+    req.files.map(async (file, index) => {
+      await sharp(file.path)
+        .resize(300, 300)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(`public/images/products/${file.filename}`);
+    })
+  );
 };
-
 
 // Export middleware functions
 export { uploadPhoto, productImgResize };
