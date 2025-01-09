2e9b87c4f679afaf345eac8a075457a3d3f494d4
eyobScript <eyobbmulugeta@gmail.com>
2025-01-09 11:49:24 +0300

    multer function

--- backend/middleware/uploadImages.js
+++ backend/middleware/uploadImages.js
@@ -9,7 +9,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-const multerStorage = multer.diskStorage({});
+const multerStorage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, path.join(__dirname, '../public/images'))
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.jpeg');
+  },
+});
 
 
 
