31a08f7feefcf7d55898e10af4acd1d6bfa69ff7
eyobScript <eyobbmulugeta@gmail.com>
2025-01-09 11:49:05 +0300

    configure our cloudinary setup

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,7 +1,6 @@
-import {v2 as cloudinary} from "cloudinary";
-import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 
-dotenv.config();
+console.log(process.env.CLOUD_NAME);
 
 // Configuration
 cloudinary.config({
@@ -12,6 +11,16 @@ cloudinary.config({
 
 // Function to upload a file to Cloudinary
 const cloudinaryUploadImage = async (fileToUpload) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(fileToUpload, (result) => {
+      resolve(
+        {
+          url: result.secure_url,
+        },
+        { resource_type: "auto" }
+      );
+    });
+  });
 };
 
 // Export the function for use in other parts of the application
