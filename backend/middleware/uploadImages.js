7249cdd9a5022152babb91e2e9e5f7d3113ef3fb
eyobScript <eyobbmulugeta@gmail.com>
2025-01-09 14:24:27 +0300

    set blogImgResize middle ware

--- backend/middleware/uploadImages.js
+++ backend/middleware/uploadImages.js
@@ -64,5 +64,36 @@ const productImgResize = async (req, res, next) => {
   }
 };
 
+// Blog Image Resizing Middleware
+
+const blogImgResize = async (req, res, next) => {
+  if (!req.files) return next();
+
+  // Define the directory for resized images
+  const productsDir = path.join(__dirname, "../public/images/blog");
+
+  try {
+    // Ensure the directory exists
+    await fs.mkdir(productsDir, { recursive: true });
+
+    // Resize and save each image
+    await Promise.all(
+      req.files.map(async (file) => {
+        await sharp(file.path)
+          .resize(300, 300)
+          .toFormat("jpeg")
+          .jpeg({ quality: 90 })
+          .toFile(path.join(productsDir, file.filename));
+          
+      })
+    );
+
+    next();
+  } catch (error) {
+    console.error("Error during image processing:", error);
+    res.status(500).send({ error: "Image processing failed" });
+  }
+};
+
 // Export middleware functions
-export { uploadPhoto, productImgResize };
+export { uploadPhoto, productImgResize, blogImgResize };
